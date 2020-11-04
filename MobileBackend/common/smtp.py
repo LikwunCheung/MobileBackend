5123c79a2361ad94b53b60113db660cb90f3bea9
zhanglihuan <lihuanz@student.unimelb.edu.au>
2020-11-04 18:08:10 +0800

    update friend event list

--- MobileBackend/common/smtp.py
+++ MobileBackend/common/smtp.py
@@ -56,7 +56,7 @@ def send_email(title: str, address: str, content: str):
         message[TO] = Header(address, UTF8)
         message[SUBJECT] = Header(title, UTF8)
 
-        connector.login(GMAIL_ACCOUNT, GMAIL_PASSWORD)
+        init_smtp()
         connector.sendmail(GMAIL_ACCOUNT, address, message.as_string())
         connector.quit()
         return True
